4f5973696f0c0cda5774b069fae840681fd8be9d
mahmoud01090 <93797271+mahmoud01090@users.noreply.github.com>
2021-12-30 23:36:18 +0200

    Add files via upload

---
+++ 7(q1).c
@@ -0,0 +1,63 @@
+
+#include<stdio.h>
+#include<malloc.h>
+void main()
+{
+
+
+  Summing implements Runnable {
+
+    int m;
+
+     Summing(int m) {
+        this.m = m;
+    }
+
+     void run() {
+        addRondom();
+    }
+
+     void addRondom() {
+        Random rand = new Random();
+        int n = rand.nextInt(10) + 1;
+        printif("number generated: " + n);
+        synchronized (this) {
+            m += n;
+        }
+    }
+}
+
+
+
+
+
+
+ static void main(String[] args) {
+    int base = 0;
+
+    Summing sum2 = new Summing(base);
+
+    Thread t1 = new Thread(sum2);
+    Thread t2 = new Thread(sum2);
+    Thread t3 = new Thread(sum2);
+    Thread t4 = new Thread(sum2);
+    Thread t5 = new Thread(sum2);
+
+    t1.start();
+    t2.start();
+    t3.start();
+    t4.start();
+    t5.start();
+
+    try {
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+        t5.join();
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+
+    System.out.print("final result: " + sum2.a);
+}
